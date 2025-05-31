e1cb0f21e8c6e739fe406ee4ff15e8245095d8b0
sourav1570 <34829501+sourav1570@users.noreply.github.com>
2025-06-01 00:25:13 +0530

    Updating files to version 3.7 This is 3.7

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Coool");   
     }
 
     // Update is called once per frame
